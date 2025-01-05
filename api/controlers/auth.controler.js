b8d005077d26de4d6c5522ea567671047fa44112
Mahesh.N.D <maheshndevraj@gmail.com>
2025-01-05 13:27:10 +0530

    added the signup feature from backend api and headers section clinet frontend

---
+++ api/controlers/auth.controler.js
@@ -0,0 +1,15 @@
+import User from "../models/user.model.js";
+import bycryptjs from "bcryptjs";
+
+export const signup = async(req, res, next)=>{
+   const {username, email, password} = req.body;
+   const hashedPassword = bycryptjs.hashSync(password,10);
+   const newUser = User({username, email, password:hashedPassword});
+   try{
+    await newUser.save();
+   res.status(201).json("User created succesfully!!");
+   }
+   catch(error){
+    next(error);
+   }
+};
