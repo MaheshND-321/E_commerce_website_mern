0722cefcd86c3ffa623c5ab9adf1a16c8edd9b77
MaheshND-321 <maheshndevraj@gmail.com>
2025-01-22 00:56:46 +0530

    added the edit of listings in backend

--- api/controlers/listing.controler.js
+++ api/controlers/listing.controler.js
@@ -30,3 +30,28 @@ export const deleteListing = async (req, res, next)=>{
     next(error);
   }
 };
+
+
+export const editListing = async(req, res, next)=>{
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing){
+        return next(errorHandeler(404, "Listing not found!"));
+    }
+
+    if (req.user.id !== listing.userRef){
+        return next(errorHandeler(401, "You can only edit you own listings."));
+    }
+
+    try{
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new :true}
+        );
+        res.status(200).json(updatedListing);
+    }
+    catch(error){
+        next(error);
+    }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createListing, deleteListing } from "../controlers/listing.controler.js";
+import { createListing, deleteListing, editListing } from "../controlers/listing.controler.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post('/create', verifyToken,createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/edit/:id', verifyToken, editListing);
 
 export default router;
